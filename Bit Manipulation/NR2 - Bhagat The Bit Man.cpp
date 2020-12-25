c2a13b1ee7b55564e213170ba44e3017d89fbd57
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2020-12-25 18:58:11 +0530

    Update NR2 - Bhagat The Bit Man.cpp

--- Bit Manipulation/NR2 - Bhagat The Bit Man.cpp
+++ Bit Manipulation/NR2 - Bhagat The Bit Man.cpp
@@ -6,7 +6,7 @@ int main(){
     int n;
     cin>>n;
     
-    ll arr[n],bits[64]={0};
+    ll arr[n],bits[64]={0}; //To store the number of 1's 
     
     for(int i=0;i<n;i++){
         cin>>arr[i];
@@ -21,7 +21,7 @@ int main(){
     }
     ll ans=0;
     for(int i=0;i<64;i++){
-        if(bits[i]!=n&&bits[i]!=0){
+        if(bits[i]!=n&&bits[i]!=0){     //If there is atleast one 1 and atleast one 0 then their XOR will give 1 and the OR will become 1
             ans=ans+(1LL<<i);
         }
     }
