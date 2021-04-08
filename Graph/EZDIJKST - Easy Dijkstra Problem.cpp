d452b6c208ea384e67975215c4021e3c13493cba
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-04-08 11:05:14 +0530

    Create EZDIJKST - Easy Dijkstra Problem.cpp

---
+++ Graph/EZDIJKST - Easy Dijkstra Problem.cpp
@@ -0,0 +1,54 @@
+//Shortest path from a src to dst using Dijkstra's Algorithm in directed graph
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define pii pair<int,int>
+ 
+int main(){
+    
+    int t;
+    cin >> t;
+    
+    while(t--){
+        int n,m;
+        cin >> n >> m;
+        
+        vector<pair<int,int>> adj[n+1];
+        
+        int a,b,w;
+        
+        while(m--){
+            cin >> a >> b >> w;
+            adj[a].push_back({b,w});
+            //adj[b].push_back({a,w});
+        }
+        
+        int src,dst;
+        cin >> src >> dst;
+        
+        priority_queue<pii, vector<pii>, greater<pii>> pq;
+        
+        vector<int> parent(n+1);
+        vector<int> dist(n+1,INT_MAX);
+        parent[src] = -1;
+        pq.push({0,src});
+        dist[src] = 0;
+        while(!pq.empty()){
+            
+            int curr = pq.top().second;
+            int curr_d = pq.top().first;
+            pq.pop();
+            
+            for(int i = 0; i < adj[curr].size(); i++){
+                
+                if(curr_d + adj[curr][i].second < dist[adj[curr][i].first]){
+                    dist[adj[curr][i].first] = curr_d + adj[curr][i].second;
+                    pq.push({dist[adj[curr][i].first],adj[curr][i].first});
+                    parent[adj[curr][i].first] = curr;
+                }
+            }
+        }
+        if(dist[dst] == INT_MAX) cout << "NO" << endl;
+        else cout << dist[dst] << endl;
+    }
+} 
